f226ba08a2042a74b268e8343ef0cb0a5819f01d
Nahid <nahidshaiket10300@gmail.com>
2019-09-24 21:23:00 +0600

    reactified

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {hot} from 'react-hot-loader';
+import './styles/App.css';
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Hello</h1>
+      <p>This is rebiton</p>
+    </div>
+  );
+}
+
+export default hot(module)(App);
